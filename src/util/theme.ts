a3c090aee66689016e1f3310f06553ae15b1f48c
Nathan Benavides-Luu <nate1299@me.com>
2021-09-17 22:44:39 -0400

    Initial commit

---
+++ src/util/theme.ts
@@ -0,0 +1,46 @@
+import {red} from '@material-ui/core/colors';
+
+const theme = {
+    components: {
+        MuiButton: {
+            defaultProps: {
+                disableElevation: true
+            },
+        },
+    },
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#4285F4',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        white: {
+            main: "#fff"
+        }
+    },
+    shape: {
+        borderRadius: 6,
+    },
+    typography: {
+        fontFamily: [
+            'IBM Plex Sans',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
+};
+
+export default theme;
